5381aeebfbe1e33ff2d6f90202f0935702ae1433
Leo FASANO <leo.fasano@cognitives-labs.com>
2020-03-26 15:20:43 +0100

    piece class

---
+++ piece.py
@@ -0,0 +1,41 @@
+class Piece:
+    def __init__(self, letter, team, envVal):
+        self.letter = letter
+        self.team = team
+        if letter != ".":
+            self.envValue = envVal
+        else:
+            self.envValue = 0
+
+    def DisplayLetter(self):
+        return self.letter
+
+    def GetTeam(self):
+        return self.team
+
+    def GetEnvValue(self):
+        return self.envValue
+    
+    def VerifyMoveLegallity(self, from_square, to_square):
+        piece = self.DisplayLetter().lower()
+        if piece == "t": # Tour
+            return from_square[0] == to_square[0] or from_square[1] == to_square[1]
+        if piece == "c": # Cavalier
+            return (abs(from_square[0] - to_square[0]) == 2 and abs(from_square[1] - to_square[1]) == 1) or (abs(from_square[1] - to_square[1]) == 2 and abs(from_square[0] - to_square[0]) == 1)
+        if piece == "f": # fou
+            return abs(from_square[0] - to_square[0]) == abs(from_square[1] - to_square[1])
+        if piece == "q": # Queen
+            return (from_square[0] == to_square[0] or from_square[1] == to_square[1]) or (abs(from_square[0] - to_square[0]) == abs(from_square[1] - to_square[1]))
+        if piece == "k": # King
+            return ((abs(from_square[0] - to_square[0]) == abs(from_square[1] - to_square[1])) and (abs(from_square[0] - to_square[0]) == 1)) or ((from_square[0] == to_square[0] and abs(from_square[1] - to_square[1]) == 1) or (from_square[1] == to_square[1] and abs(from_square[0] - to_square[0]) == 1))
+        if piece == "p": # pion
+            if givenPiece.GetTeam() == 0:
+                if from_square[1] == 1:
+                    return from_square[0] == to_square[0] and (from_square[1] - to_square[1] == -1 or from_square[1] - to_square[1] == -2)
+                else:
+                    return from_square[0] == to_square[0] and from_square[0] - to_square[0] == -1 
+            else: 
+                if from_square[1] == 6:
+                    return from_square[0] == to_square[0] and (from_square[1] - to_square[1] == 1 or from_square[1] - to_square[1] == 2)
+                else:
+                    return from_square[0] == to_square[0] and from_square[1] - to_square[1] == 1 
